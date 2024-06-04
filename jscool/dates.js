d5ef9e8bfc927aa64e07504a54936c26fb6508c3
Sanchit agrawal <125626091+sanxxit@users.noreply.github.com>
2024-06-04 15:08:22 +0000

    hey

---
+++ jscool/dates.js
@@ -0,0 +1,13 @@
+//dates
+ let mydate= new Date()
+ console.log(mydate)//weird looking 2024-06-04T15:01:13.165Z
+ console.log(mydate.toDateString())
+ console.log(mydate.toISOString())
+ console.log(mydate.toJSON())
+ console.log(mydate.toLocaleDateString())
+ console.log(mydate.toString())
+ console.log(mydate.toTimeString())
+ console.log(mydate.toUTCString())
+ console.log(mydate.getTimezoneOffset())
+ console.log(mydate.getHours())
+ console.log(mydate.getMonth())
